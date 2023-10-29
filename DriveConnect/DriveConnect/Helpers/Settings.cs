e831a51bd49f16b751f3b6c9493d7b8811b52b02
flaubertlekhem <109031049+flaubertlekhem@users.noreply.github.com>
2023-10-29 09:35:27 +0100

    For microsoft bounty

--- DriveConnect/DriveConnect/Helpers/Settings.cs
+++ DriveConnect/DriveConnect/Helpers/Settings.cs
@@ -7,9 +7,7 @@ namespace DriveConnect.Helpers
 {
     public static class Settings
     {
-        public static string OneDriveAppId { get; } = "d9ff4603-ebf0-47e1-bc24-a3d1b78c368d";
-        public static string OneDriveObjectId { get; } = "551e097a-097b-4c84-8596-d43ccfcd98da";
-        public static string OneDriveTenantId { get; } = "b05e5dbc-60da-4e59-b1ed-16beda9e5683";
+        public static string OneDriveAppId { get; } = "YOUR APP ID";
         public static string OneDriveRedirectURI { get; } = "https://login.microsoftonline.com/common/oauth2/nativeclient";
         public static string[] Scopes { get; } =
         {
@@ -35,7 +33,6 @@ namespace DriveConnect.Helpers
             OneDriveConnected = false;
             FileExtensionHelper.ClearAllTempFiles();
             FileExtensionHelper.PeriodicFileClear();
-            //OneDriveUserInfo = string.Empty;
         }
 
         // 0 = default, 1 = light, 2 = dark
